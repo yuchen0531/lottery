e62e99e2a6b4f027a66b3068df343dbe043a9099
yuchen0531 <dg5202dg5202@gmail.com>
2025-12-16 16:28:35 +0800

    刪除圖片調整樣式加上banner

--- src/pages/MessageRafflePage.tsx
+++ src/pages/MessageRafflePage.tsx
@@ -4,6 +4,8 @@ import { db } from "../lib/firebase"
 import { onValue, ref } from "firebase/database"
 import { motion } from "framer-motion"
 import { LoadingModal } from "../components/LoadingComponents"
+import banner from '../assets/images/banner.png'
+import close from "../assets/images/close.png"
 
 type Message = {
   id: string
@@ -28,6 +30,7 @@ export default function MessageRafflePage() {
   const rafflePoolRef = useRef<Message[]>([])
   const [isShuffling, setIsShuffling] = useState(false)
   const [fakeDisplay, setFakeDisplay] = useState<Message | null>(null)
+  const [isOpenModel, setIsOpenModel] = useState(false)
 
   const unsubscribeRef = useRef<(() => void) | null>(null)
 
@@ -76,7 +79,7 @@ export default function MessageRafflePage() {
       alert("抽獎池沒有剩餘的留言了！");
       return;
     }
-
+    setIsOpenModel(true)
     // 🔥 1) 開始假滾動動畫
     setIsShuffling(true)
 
@@ -106,6 +109,7 @@ export default function MessageRafflePage() {
 
   return (
     <div className="bg-[#a19887] p-4 min-h-screen relative">
+      <img src={banner} className="mb-6 w-full max-w-[900px] mx-auto" alt="" />
       <div className="text-center mb-4">
         <button
           onClick={hasStarted ? handleRaffle : handleStart}
@@ -114,46 +118,51 @@ export default function MessageRafflePage() {
           {hasStarted ? "開始抽獎" : "截止抽獎名單"}
         </button>
       </div>
-        {/* 🎰 假滾動動畫 */}
-    {isShuffling && fakeDisplay && (
-      <motion.div
-        key="shuffle"
-        initial={{ opacity: 0, y: 10 }}
-        animate={{ opacity: 1, y: 0 }}
-        exit={{ opacity: 0 }}
-        transition={{ duration: 0.2 }}
-        className="text-center mb-6"
-      >
-        <div className="text-3xl font-bold text-white drop-shadow mb-2">
-          {fakeDisplay.name || fakeDisplay.lineDisplayname}
-        </div>
+        {isOpenModel && (
+          <div className="model bg-[#534d46]">
+            {isShuffling && fakeDisplay && (
+              <motion.div
+                key="shuffle"
+                initial={{ opacity: 0, y: 10 }}
+                animate={{ opacity: 1, y: 0 }}
+                exit={{ opacity: 0 }}
+                transition={{ duration: 0.2 }}
+                className="text-center mb-6"
+              >
+                <div className="text-3xl font-bold text-white drop-shadow mb-2">
+                  {fakeDisplay.name || fakeDisplay.lineDisplayname}
+                </div>
 
-        <div className="mx-auto max-w-md bg-white/80 border border-[#e6d4b0] rounded-xl p-3 shadow">
-          <div className="text-[#5f554c] text-base leading-relaxed">
-            {fakeDisplay.text}
+                <div className="mx-auto max-w-md bg-white border border-[#e6d4b0] rounded-xl p-3 shadow">
+                  <div className="text-[#5f554c] text-base leading-relaxed">
+                    {fakeDisplay.text}
+                  </div>
+                </div>
+              </motion.div>
+            )}
+            {!isShuffling && revealedWinner && (
+              <>
+              <div className="text-center mb-6 winner-card">
+                <div className="text-5xl font-extrabold golden-flash mb-4 tracking-wide leading-relaxed">
+                  {revealedWinner.name || revealedWinner.lineDisplayname}
+                </div>
+
+                {/* 留言內容卡片 */}
+                <div className="mx-auto max-w-md bg-white border-4 border-[#e6d4b0] rounded-2xl p-5 shadow-lg">
+                  <div className="text-[#6b5f52] text-lg leading-relaxed">
+                    {revealedWinner.text}
+                  </div>
+                </div>
+              </div>
+              <button onClick={() => setIsOpenModel(false)} className="absolute bottom-[20%] font-bold text-3xl w-[40px] h-[40px] p-3 bg-white text-[#272726] rounded-full flex items-center justify-center items-center">
+                <img src={close} className="w-full" alt="Close" />
+              </button>
+              </>
+            )}
           </div>
-        </div>
-      </motion.div>
-    )}
-
-      {!isShuffling && revealedWinner && (
-        <div className="text-center mb-6 winner-card">
-          <div className="text-5xl font-extrabold golden-flash mb-4 tracking-wide leading-relaxed">
-            {revealedWinner.name || revealedWinner.lineDisplayname}
-          </div>
-
-          {/* 留言內容卡片 */}
-          <div className="mx-auto max-w-md bg-white/95 border border-[#e6d4b0] rounded-2xl p-5 shadow-lg">
-            <div className="text-[#6b5f52] text-lg leading-relaxed">
-              {revealedWinner.text}
-            </div>
-          </div>
-        </div>
-      )}
-
-
+        )}
       <h2 className="text-white font-bold text-2xl mb-2 text-center">中獎名單</h2>
-      <div className="bg-[#fbf4eb] p-4 rounded-2xl shadow-md mb-6 max-w-xl mx-auto">
+      <div className="bg-[#fbf4eb] p-4 rounded-2xl shadow-md mb-6 max-w-[900px] mx-auto">
         {winnerList.length === 0 ? (
           <div className="text-[#857d71]">目前尚無中獎者</div>
         ) : (
@@ -178,7 +187,7 @@ export default function MessageRafflePage() {
       </div>
 
       <h2 className="text-white font-bold text-2xl mb-2 text-center">留言列表</h2>
-      <div className="bg-[#fbf4eb] p-4 rounded-2xl shadow-md  max-w-xl mx-auto">
+      <div className="bg-[#fbf4eb] p-4 rounded-2xl shadow-md  max-w-[900px] mx-auto">
         <ul className="space-y-3">
           {messages.map(m => (
             <li key={m.id}>
